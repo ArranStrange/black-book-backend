0c3485f9d1331e4a549785f35305d5cd37ac0d95
Arran <arranstrange@googlemail.com>
2025-03-10 15:59:32 +0000

    auth reverted

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -10,42 +10,37 @@ export const register = async (req, res) => {
     // Check if username exists
     const existingUser = await User.findOne({ username });
     if (existingUser) {
-      return res
-        .status(400)
-        .json({ status: "error", message: "Username already taken" });
+      return res.json({ status: "error", message: "Username already taken" });
     }
 
     // Hash password
-    //10 represents the "Salt Rounds", this is how many times the function is run on the password, the more times the more secure
+    //10 represents the "Salt Rounds", this is how many times the function is run on the password, the more times the more
     const hashedPassword = await bcrypt.hash(password, 10);
-
     const newUser = new User({ username, password: hashedPassword });
-
     const savedUser = await newUser.save();
-    res
-      .status(201)
-      .json({ message: "User registered successfully", user: savedUser });
+
+    res.json({ message: "User registered successfully", user: savedUser });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.json({ message: error.message });
   }
 };
 
 // User login
 export const login = async (req, res) => {
-  const { username, password } = req.body;
+  const { username, password } = req.query;
   try {
     const user = await User.findOne({ username });
     if (!user) {
-      return res.status(401).json({ message: "Invalid username or password" });
+      return res.json({ message: "Invalid username or password" });
     }
 
     const isMatch = await bcrypt.compare(password, user.password);
     if (isMatch) {
-      res.status(200).json({ message: "Login successful" });
+      res.json({ message: "Login successful" });
     } else {
-      res.status(401).json({ message: "Invalid username or password" });
+      res.json({ message: "Invalid username or password" });
     }
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.json({ message: error.message });
   }
 };
